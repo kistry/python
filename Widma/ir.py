b40aa3e99588621a8b3fbf03b8868a7b3bc535d9
Sławomir <kistry@o2.pl>
2020-04-10 15:01:38 +0200

    Update ir.py

--- Widma/ir.py
+++ Widma/ir.py
@@ -2,9 +2,8 @@ with open('ir.txt') as file:
     y = [
         line.split()[-2]
         for line in file
-        if '#' in line:
-        else
-        
+        if '#' in line
+             
         ]
         
 print(y[1:])
